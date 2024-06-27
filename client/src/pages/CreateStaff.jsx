7656e745d3328c5dff9f5dcac561ca2687eba500
Jake Onquit <148852640+sunrayyourdev@users.noreply.github.com>
2024-06-28 01:04:18 +0800

    added mui components   to CreateStaff page

--- client/src/pages/CreateStaff.jsx
+++ client/src/pages/CreateStaff.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Box, Typography, TextField, Button } from '@mui/material';
 
 function CreateStaff() {
-  return (
-    <div>Create Staff</div>
-  )
+    return (
+        <Box>
+            <Typography variant="h5" sx={{ my: 2 }}>
+                Add Tutorial
+            </Typography>
+            <Box component="form">
+                <TextField
+                    fullWidth
+                    margin="dense"
+                    autoComplete="off"
+                    label="Title"
+                    name="title"
+                />
+                <TextField
+                    fullWidth
+                    margin="dense"
+                    autoComplete="off"
+                    multiline
+                    minRows={2}
+                    label="Description"
+                    name="description"
+                />
+                <Box sx={{ mt: 2 }}>
+                    <Button variant="contained" type="submit">
+                        Add
+                    </Button>
+                </Box>
+            </Box>
+        </Box>
+    )
 }
 
 export default CreateStaff
