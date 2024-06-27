c9b8bc2fffac77d89dfa13975bcf546a61728d0f
Jake Onquit <148852640+sunrayyourdev@users.noreply.github.com>
2024-06-27 14:53:08 +0800

    added phone_number and salary columns to model

--- server/models/staff.js
+++ server/models/staff.js
@@ -8,8 +8,16 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING(100),
             allowNull: false
         },
+        phone_number: {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
         role: {
             type: DataTypes.TEXT,
+            allowNull: false
+        },
+        salary: {
+            type: DataTypes.DECIMAL(10, 2),
             allowNull: false
         }
     }, {
