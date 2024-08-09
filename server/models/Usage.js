7834bc8642d71b537666336d0c971cdf950bc494
ImranZak <105257918+ImranZak@users.noreply.github.com>
2024-08-09 13:05:40 +0800

    Updated File structure to be mergeable with main
    Fixed previous issue with the main having a client, server structure while the feedback branch had another folder called Staff_and_user where there was another client and server folder inside, meaning changes couldn't be merged into one main program

--- server/models/Usage.js
+++ server/models/Usage.js
@@ -1,24 +1,34 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../utils/database');
+const { Model, DataTypes } = require('sequelize');
 
-const Usage = sequelize.define('Usage', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    value: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-    },
-    timestamp: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-    },
-});
+class Usage extends Model {
+    static init(sequelize) {
+        return super.init({
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            type: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            value: {
+                type: DataTypes.FLOAT,
+                allowNull: false,
+            },
+            timestamp: {
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
+            }
+        }, {
+            sequelize,
+            tableName: 'usage',
+        });
+    }
+
+    static associate(models) {
+        // Define associations here if necessary
+    }
+}
 
 module.exports = Usage;
