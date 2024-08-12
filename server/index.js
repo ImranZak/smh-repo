1d04cbeb9c670e6af5175a02572045becef1baaf
JelatSigaPro <jeraldliu06@gmail.com>
2024-08-12 09:30:58 +0800

    Revert "Educational Module V6"
    This reverts commit 8c1f81a18d63d27b9a3345f2b4b6ca6ddb6c0a81.

--- server/index.js
+++ server/index.js
@@ -36,7 +36,6 @@ const signupRoute = require('./routes/signup');
 const userRoute = require('./routes/user');
 const staffRoute = require('./routes/staff');
 const markerRoute = require('./routes/marker');
-const sendquizemailRoute = require('./routes/quizEmail');
 
 // Routes from Feedback branch
 const dataFeedback = require('./routes/datafeedback');
@@ -60,7 +59,6 @@ app.use("/api/user", userRoute);
 app.use("/datafeedback", dataFeedback);
 app.use('/staff', staffRoute)
 app.use('/marker', markerRoute)
-app.use('/sendquizemail', sendquizemailRoute)
 
 console.log('Routes have been set up.');
 
--- server/middlewares/email.js
+++ server/middlewares/email.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer');
-// hugging face password: N@20Leoajf*4
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'students4nyp@gmail.com', // Your email address
-        pass: 'xkxa qzeh yydf xpnc'  // Your email password
+        pass: 'W3L0V3nyP.3023'  // Your email password
     }
 });
 
@@ -13,11 +13,10 @@ const sendQuizResultEmail = (to, quizTitle, score) => {
         from: 'students4nyp@gmail.com',
         to: to,
         subject: `Your Quiz Result for ${quizTitle}`,
-        html: `<h1>Singapore Management Hub</h1><p>DO NOT RESPOND TO THIS EMAIL AS IT IS AUTO GENERATED! <br>You have completed the quiz "${quizTitle}" with a score of ${score}%.<br>Sent on: ${new Date().toLocaleString()}<p>`,
+        html: `<h1>Singapore Management Hub</h1><p>DO NOT RESPOND TO THIS EMAIL AS IT IS AUTO GENERATED! <br>You have completed the quiz "${quizTitle}" with a score of ${score}%.<p>`
     };
 
     return transporter.sendMail(mailOptions);
 };
-
 
 module.exports = sendQuizResultEmail;
